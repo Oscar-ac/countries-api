81db22f6d208954d62001ffa0c8c5259ae5a6ca6
Oscar A.C <oangelkovcummings@gmail.com>
2023-02-07 15:40:11 +0900

    Updates

---
+++ precache-manifest.a50c0ec2af07df4f9135958fd61db8cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "139466357b0486bc0dde",
+    "url": "/countries-api/static/css/main.4c49b183.chunk.css"
+  },
+  {
+    "revision": "139466357b0486bc0dde",
+    "url": "/countries-api/static/js/main.13946635.chunk.js"
+  },
+  {
+    "revision": "de777e5fde807a0452ce",
+    "url": "/countries-api/static/js/1.de777e5f.chunk.js"
+  },
+  {
+    "revision": "9d25e29535730d1d54fb",
+    "url": "/countries-api/static/css/2.2701832c.chunk.css"
+  },
+  {
+    "revision": "9d25e29535730d1d54fb",
+    "url": "/countries-api/static/js/2.9d25e295.chunk.js"
+  },
+  {
+    "revision": "fc9bb698dbf2cb719384",
+    "url": "/countries-api/static/js/runtime~main.fc9bb698.js"
+  },
+  {
+    "revision": "dd99ca4d09ce0e85778b88ca0b17a5e2",
+    "url": "/countries-api/index.html"
+  }
+];
